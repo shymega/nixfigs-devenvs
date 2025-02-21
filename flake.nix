71947942b31394d6312b0180c572f1a11e02b9a9
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-21 22:19:04 +0000

    chore: Update Flake inputs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- flake.nix
+++ flake.nix
@@ -32,55 +32,51 @@
     the-nix-way.url = "github:the-nix-way/dev-templates";
   };
 
-  outputs =
-    { self, ... }@inputs:
-    let
-      genPkgs = system: import inputs.nixpkgs { inherit system; };
+  outputs = {self, ...} @ inputs: let
+    genPkgs = system: import inputs.nixpkgs {inherit system;};
 
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
 
-      treeFmtEachSystem =
-        f: inputs.nixpkgs.lib.genAttrs systems (system: f inputs.nixpkgs.legacyPackages.${system});
-      treeFmtEval = treeFmtEachSystem (
-        pkgs:
-        inputs.nixfigs-helpers.inputs.treefmt-nix.lib.evalModule pkgs "${inputs.nixfigs-helpers.helpers.formatter
+    treeFmtEachSystem = f: inputs.nixpkgs.lib.genAttrs systems (system: f inputs.nixpkgs.legacyPackages.${system});
+    treeFmtEval = treeFmtEachSystem (
+      pkgs:
+        inputs.nixfigs-helpers.inputs.treefmt-nix.lib.evalModule pkgs "${
+          inputs.nixfigs-helpers.helpers.formatter
         }"
-      );
+    );
 
-      forEachSystem = inputs.nixpkgs.lib.genAttrs systems;
-    in
-    {
-      # for `nix fmt`
-      formatter = treeFmtEachSystem (pkgs: treeFmtEval.${pkgs.system}.config.build.wrapper);
-      # for `nix flake check`
-      checks =
-        treeFmtEachSystem (pkgs: {
-          formatting = treeFmtEval.${pkgs}.config.build.wrapper;
-        })
-        // forEachSystem (system: {
-          pre-commit-check = import "${inputs.nixfigs-helpers.helpers.checks}" {
-            inherit self system;
-            inherit (inputs.nixfigs-helpers) inputs;
-            inherit (inputs.nixpkgs) lib;
-          };
-        });
-      devShells = forEachSystem (
-        system:
-        let
-          pkgs = genPkgs system;
-        in
-        import inputs.nixfigs-helpers.helpers.devShells { inherit pkgs self system; }
-      );
-
-      templates-srcs = rec {
-        shymega = {
-          default = inputs.the-nix-way.templates.empty;
+    forEachSystem = inputs.nixpkgs.lib.genAttrs systems;
+  in {
+    # for `nix fmt`
+    formatter = treeFmtEachSystem (pkgs: treeFmtEval.${pkgs.system}.config.build.wrapper);
+    # for `nix flake check`
+    checks =
+      treeFmtEachSystem (pkgs: {
+        formatting = treeFmtEval.${pkgs}.config.build.wrapper;
+      })
+      // forEachSystem (system: {
+        pre-commit-check = import "${inputs.nixfigs-helpers.helpers.checks}" {
+          inherit self system;
+          inherit (inputs.nixfigs-helpers) inputs;
+          inherit (inputs.nixpkgs) lib;
         };
-        all-templates = shymega // inputs.the-nix-way.templates;
+      });
+    devShells = forEachSystem (
+      system: let
+        pkgs = genPkgs system;
+      in
+        import inputs.nixfigs-helpers.helpers.devShells {inherit pkgs self system;}
+    );
+
+    templates-srcs = rec {
+      shymega = {
+        default = inputs.the-nix-way.templates.empty;
       };
-      templates = self.templates-srcs.all-templates;
+      all-templates = shymega // inputs.the-nix-way.templates;
     };
+    templates = self.templates-srcs.all-templates;
+  };
 }
