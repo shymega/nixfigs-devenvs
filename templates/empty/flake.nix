49fa6cb74af2b71b9a5bfe84547c5888c5a4f001
Dom Rodriguez <shymega@shymega.org.uk>
2025-11-30 21:16:35 +0000

    chore: Update Flake inputs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- templates/empty/flake.nix
+++ templates/empty/flake.nix
@@ -5,20 +5,27 @@
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
   # Flake outputs
-  outputs = {nixpkgs}: let
-    # The systems supported for this flake
-    supportedSystems = [
-      "x86_64-linux" # 64-bit Intel/AMD Linux
-      "aarch64-linux" # 64-bit ARM Linux
-      "x86_64-darwin" # 64-bit Intel macOS
-      "aarch64-darwin" # 64-bit ARM macOS
-    ];
+  outputs = inputs: let
+    forEachSystem = let
+      # The systems supported for this flake
+      supportedSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
 
-    # Helper to provide system-specific attributes
-    forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {pkgs = import nixpkgs {inherit system;};});
+      genPkgs = let
+        inherit (inputs) nixpkgs;
+      in
+        system: nixpkgs.legacyPackages.${system};
+
+      inherit (inputs.nixpkgs.lib) genAttrs;
+    in
+      f: genAttrs supportedSystems (system: f (genPkgs system));
   in {
-    devShells = forEachSupportedSystem (
-      {pkgs}: {
+    devShells = forEachSystem (
+      pkgs: {
         default = pkgs.mkShell {
           # The Nix packages provided in the environment
           # Add any you need here
